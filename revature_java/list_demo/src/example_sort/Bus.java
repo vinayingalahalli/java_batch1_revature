36feaacd020034dfa902590a0b05be4593d4b57d
vinayingalhalli <vinay.ingalahalli1989@gmail.com>
2019-08-15 22:44:05 +0530

    updated till list

---
+++ revature_java/list_demo/src/example_sort/Bus.java
@@ -0,0 +1,58 @@
+package example_sort;
+
+public class Bus {
+	private int id;
+	private String name;
+	private double fare;
+	private float ratings;
+
+	public Bus() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Bus(int id, String name, double fare, float ratings) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.fare = fare;
+		this.ratings = ratings;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getFare() {
+		return fare;
+	}
+
+	public void setFare(double fare) {
+		this.fare = fare;
+	}
+
+	public float getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(float ratings) {
+		this.ratings = ratings;
+	}
+
+	@Override
+	public String toString() {
+		return "Bus [id=" + id + ", name=" + name + ", fare=" + fare + ", ratings=" + ratings + "]";
+	}
+
+}
