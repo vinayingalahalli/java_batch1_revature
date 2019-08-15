39304577a80ce7fc38df2068858ea542f7377154
vinayingalhalli <vinay.ingalahalli1989@gmail.com>
2019-08-16 03:02:55 +0530

    updated till iterator

---
+++ revature_java/set_demo/src/example_sort/Bus.java
@@ -0,0 +1,87 @@
+package example_sort;
+
+public class Bus{
+	private int id;
+	private String name;
+	private double fare;
+	private float ratings;
+
+	public Bus() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Bus(int id, String name, double fare, float ratings) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.fare = fare;
+		this.ratings = ratings;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getFare() {
+		return fare;
+	}
+
+	public void setFare(double fare) {
+		this.fare = fare;
+	}
+
+	public float getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(float ratings) {
+		this.ratings = ratings;
+	}
+
+	@Override
+	public String toString() {
+		return "Bus [id=" + id + ", name=" + name + ", fare=" + fare + ", ratings=" + ratings + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bus other = (Bus) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	/*
+	 * @Override public int compareTo(Bus o) { Integer i1=this.id; Integer i2=o.id;
+	 * 
+	 * return i1.compareTo(i2); }
+	 */
+	
+
+}
