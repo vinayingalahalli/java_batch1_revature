1a60ed537a896d3ba248893464cd8be99206db6d
vinayingalhalli <vinay.ingalahalli1989@gmail.com>
2019-08-21 02:52:34 +0530

    updated till exception

---
+++ revature_java/exception_demo/src/com/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.exception;
+//checked
+public class BusinessException extends Exception {
+
+	public BusinessException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public BusinessException(final String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
