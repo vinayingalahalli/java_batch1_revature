ea8f447ad16278a844dbfef4b22befa700e2a5d9
vinayingalhalli <vinay.ingalahalli1989@gmail.com>
2019-08-22 00:51:49 +0530

    updated till junit

--- revature_java/product_app_v2.0/src/com/business/impl/ProductBusinessImpl.java
+++ revature_java/product_app_v2.0/src/com/business/impl/ProductBusinessImpl.java
@@ -1,6 +1,7 @@
 package com.business.impl;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.business.ProductBusiness;
@@ -9,4 +10,46 @@ import com.pojo.Product;
 public class ProductBusinessImpl implements ProductBusiness{
 
 	private Map<Integer, Product> productMap=new HashMap<>();
+
+	@Override
+	public Product addProduct(Product product) {
+		
+		if(productMap.containsKey(product.getPid())) {
+			return null;
+		}else {
+			productMap.put(product.getPid(), product);
+			return product;
+		}
+		
+	}
+
+	@Override
+	public Product updateProductPrice(int pid, double newPrice) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteProduct(int pid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Product getProductById(int pid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getProductsByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
