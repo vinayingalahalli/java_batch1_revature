9b50b7a9a4ef768542a4e5a58364efce717e0a61
vinayingalhalli <vinay.ingalahalli1989@gmail.com>
2019-09-25 20:31:34 +0530

    spring inject collections

--- revature_java/spring_demo/src/com/model/Player.java
+++ revature_java/spring_demo/src/com/model/Player.java
@@ -1,5 +1,7 @@
 package com.model;
 
+import java.util.List;
+
 public class Player {
 	public Player() {
 		// TODO Auto-generated constructor stub
@@ -7,35 +9,51 @@ public class Player {
 
 	private int id;
 	private String name;
-	private Team team;
+	private List<Team> teamList;
+	
 	
 	public int getId() {
 		return id;
 	}
+
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
+
 	public String getName() {
 		return name;
 	}
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
+	public List<Team> getTeamList() {
+		return teamList;
+	}
+
+
+	public void setTeamList(List<Team> teamList) {
+		this.teamList = teamList;
+	}
+
+
 	public Player(int id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
+
+
 	@Override
 	public String toString() {
-		return "Player [id=" + id + ", name=" + name + ", team=" + team + "]";
+		return "Player [id=" + id + ", name=" + name + ", teamList=" + teamList + "]";
 	}
-	public Team getTeam() {
-		return team;
-	}
-	public void setTeam(Team team) {
-		this.team = team;
-	}
+	
 	
 
 }
